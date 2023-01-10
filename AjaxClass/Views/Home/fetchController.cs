aa5384e17222ff8c73278cec64f60a887b375f43
Yi-Ming-Lin <114230883+Yi-Ming-Lin@users.noreply.github.com>
2023-01-10 11:15:07 +0800

    fetch

---
+++ AjaxClass/Views/Home/fetchController.cs
@@ -0,0 +1,27 @@
+﻿using AjaxClass.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxClass.Views.Home
+{
+    public class fetchController : Controller
+    {
+        AjaxContext db = new AjaxContext();
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult city()
+        {
+            return Json(db.Addresses.Select(x=>x.City).Distinct());
+        }
+        public IActionResult state(string city)
+        {
+            return Json(db.Addresses.Where(x => x.City == city).Select(x => x.SiteId).Distinct());
+        }
+        public IActionResult road(string state)
+        {
+            return Json(db.Addresses.Where(x => x.SiteId == state).Select(x => x.Road).Distinct());
+        }
+    }
+}
